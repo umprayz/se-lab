a374832435306baafabf1f3ade2850fa66123e06
Dávid Hegedüs <david@arxnet.hu>
2024-10-03 12:29:19 +0200

    Fixed generator (Random class entity)

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,7 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  private Random generator;
+  private Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos) {
     this.torpedoCount = numberOfTorpedos;
